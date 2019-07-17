c50091a8cf0124a39453b367dcf6a8ee29572c38
gfb <2545188523@qq.com>
2019-07-17 16:44:43 +0800

    first1

---
+++ src/components/Welcome.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="welcome">
+      <h1>{{welcome_yu}}</h1>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+   name:'Welcome',
+   data() {
+     return{
+     welcome_yu:"欢迎来到缘成" 
+     }
+   }
+
+}
+</script>
+
+
+<style lang="less" scoped>
+
+ 
+
+</style>
+
